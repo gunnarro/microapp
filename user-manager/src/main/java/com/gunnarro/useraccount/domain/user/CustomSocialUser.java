2cca51f03afcb031c771b567479030533bc524cf
mentos <mentos@localhost.localdomain>
2018-11-03 23:03:42 +0100

    microapp pom update

--- user-manager/src/main/java/com/gunnarro/useraccount/domain/user/CustomSocialUser.java
+++
@@ -1,9 +0,0 @@
-package com.gunnarro.useraccount.domain.user;
-
-import org.springframework.social.security.SocialUserDetails;
-
-public class CustomSocialUser extends LocalUser implements SocialUserDetails {
-
-    private static final long serialVersionUID = 1L;
-
-}
